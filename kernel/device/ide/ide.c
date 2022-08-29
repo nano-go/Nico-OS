cfed5cad29ed7751629e4db75a19a4633d7c16bb
nano-go <1522862112@qq.com>
2022-08-29 06:52:52 +0000

    Fix a bug that make a looped idequeue when multiple tasks call iderw at the same time.
        Format code.

--- kernel/device/ide/ide.c
+++ kernel/device/ide/ide.c
@@ -1,6 +1,6 @@
+#include "kernel/ide.h"
 #include "kernel/buf.h"
 #include "kernel/debug.h"
-#include "kernel/ide.h"
 #include "kernel/iopic.h"
 #include "kernel/trap.h"
 #include "kernel/x86.h"
@@ -16,22 +16,22 @@ extern "C" {
  * This file implements a simple PIO-based(Programmed I/O) IDE driver
  */
 
-#define PORT_DATA(ide_chan)        ((ide_chan)->port_base + 0)
-#define PORT_ERROR(ide_chan)       ((ide_chan)->port_base + 1)
-#define PORT_SEC_CNT(ide_chan)     ((ide_chan)->port_base + 2)
-#define PORT_LBA_L(ide_chan)       ((ide_chan)->port_base + 3)
-#define PORT_LBA_M(ide_chan)       ((ide_chan)->port_base + 4)
-#define PORT_LBA_H(ide_chan)       ((ide_chan)->port_base + 5)
-#define PORT_DEV(ide_chan)         ((ide_chan)->port_base + 6)
-#define PORT_STATUS(ide_chan)      ((ide_chan)->port_base + 7)
-#define PORT_CMD(ide_chan)         (PORT_STATUS(ide_chan))
-#define PORT_ALT_STATUS(ide_chan)  ((ide_chan)->port_base + 0x206)
-#define PORT_CTL(ide_chan)         (PORT_ALT_STATUS(ide_chan))
+#define PORT_DATA(ide_chan)		  ((ide_chan)->port_base + 0)
+#define PORT_ERROR(ide_chan)	  ((ide_chan)->port_base + 1)
+#define PORT_SEC_CNT(ide_chan)	  ((ide_chan)->port_base + 2)
+#define PORT_LBA_L(ide_chan)	  ((ide_chan)->port_base + 3)
+#define PORT_LBA_M(ide_chan)	  ((ide_chan)->port_base + 4)
+#define PORT_LBA_H(ide_chan)	  ((ide_chan)->port_base + 5)
+#define PORT_DEV(ide_chan)		  ((ide_chan)->port_base + 6)
+#define PORT_STATUS(ide_chan)	  ((ide_chan)->port_base + 7)
+#define PORT_CMD(ide_chan)		  (PORT_STATUS(ide_chan))
+#define PORT_ALT_STATUS(ide_chan) ((ide_chan)->port_base + 0x206)
+#define PORT_CTL(ide_chan)		  (PORT_ALT_STATUS(ide_chan))
 
-#define IDE_BSY  0b10000000
+#define IDE_BSY	 0b10000000
 #define IDE_DRDY 0b01000000
-#define IDE_DF   0b00100000
-#define IDE_ERR  0b00000001
+#define IDE_DF	 0b00100000
+#define IDE_ERR	 0b00000001
 
 #define CMD_READ  0x20
 #define CMD_WRITE 0x30
@@ -39,10 +39,10 @@ extern "C" {
 #define CMD_WRMUL 0xC5
 
 #define GET_CMD_WRITE(secs) (((secs) == 1) ? CMD_WRITE : CMD_WRMUL)
-#define GET_CMD_READ(secs)  (((secs) == 1) ? CMD_READ : CMD_RDMUL)
+#define GET_CMD_READ(secs)	(((secs) == 1) ? CMD_READ : CMD_RDMUL)
 
-#define BIT_DEV_MBS   0B10100000
-#define BIT_DEV_LBA   0B01000000
+#define BIT_DEV_MBS	  0B10100000
+#define BIT_DEV_LBA	  0B01000000
 #define BIT_DEV_SLAVE 0B00010000
 
 /**
@@ -50,10 +50,8 @@ extern "C" {
  */
 #define HARD_DISK_CNT ((uint8_t)(*((uint8_t *) 0x475)))
 
-struct ide_channel ide_chans[2] = {
-	{.ide_chan_id = 0, .port_base = 0x1f0, .irq_no = 0xe},
-	{.ide_chan_id = 1, .port_base = 0x170, .irq_no = 0xf}
-};
+struct ide_channel ide_chans[2] = {{.ide_chan_id = 0, .port_base = 0x1f0, .irq_no = 0xe},
+								   {.ide_chan_id = 1, .port_base = 0x170, .irq_no = 0xf}};
 
 static struct spinlock idelock;
 static struct buf *idequeue;
@@ -62,7 +60,7 @@ static struct disk *cur_disk = NULL;
 
 /**
  * Setup the LBA disk address and the number of sectors to be operated.
- */ 
+ */
 static inline void select_secs(struct disk *disk, uint32_t lba, uint32_t secs) {
 	struct ide_channel *ide_chan = disk->ide_chan;
 	outb(PORT_ALT_STATUS(disk->ide_chan), 0); // generate interrupt.
@@ -70,8 +68,7 @@ static inline void select_secs(struct disk *disk, uint32_t lba, uint32_t secs) {
 	outb(PORT_LBA_L(ide_chan), lba & 0xFF);
 	outb(PORT_LBA_M(ide_chan), (lba >> 8) & 0xFF);
 	outb(PORT_LBA_H(ide_chan), (lba >> 16) & 0xFF);
-	outb(PORT_DEV(ide_chan), BIT_DEV_LBA | BIT_DEV_MBS |
-								 ((disk->dev_no == 1) ? BIT_DEV_SLAVE : 0) |
+	outb(PORT_DEV(ide_chan), BIT_DEV_LBA | BIT_DEV_MBS | ((disk->dev_no == 1) ? BIT_DEV_SLAVE : 0) |
 								 ((lba >> 24) & 0xF));
 }
 
@@ -88,15 +85,15 @@ static int idewait(struct ide_channel *ide_chan) {
 }
 
 /**
- * Start to syncronize the buf.
+ * Start to synchronize the buf.
  */
 static void idestart(struct buf *buf) {
 	ASSERT(buf != NULL);
-	
+
 	struct ide_channel *ide_chan = buf->disk->ide_chan;
 	int sectors_per_block = BLOCK_SIZE / SECTOR_SIZE;
 	int sector_lba = buf->block_no * sectors_per_block;
-	
+
 	ASSERT(sectors_per_block < 8);
 
 	select_secs(buf->disk, sector_lba, sectors_per_block);
@@ -115,18 +112,20 @@ static void idestart(struct buf *buf) {
  * IDE interrupt handler.
  */
 static void ide_intr_handler(struct trap_frame *tf) {
-	ASSERT(tf->intr_nr == IRQ_START_VEC_NR + 0xe ||
-		   tf->intr_nr == IRQ_START_VEC_NR + 0xf);
+	ASSERT(tf->intr_nr == IRQ_START_VEC_NR + 0xe || tf->intr_nr == IRQ_START_VEC_NR + 0xf);
 
 	bool int_save;
 	spinlock_acquire(&idelock, &int_save);
 
 	uint8_t ide_chan_idx = tf->intr_nr - (IRQ_START_VEC_NR + 0xe);
 	struct ide_channel *ide_chan = &ide_chans[ide_chan_idx];
-		
+
 	struct buf *buf = idequeue;
-	ASSERT(buf != NULL);
-	
+	if (buf == NULL) {
+		spinlock_release(&idelock, &int_save);
+		return;
+	}
+
 	if ((buf->flags & BUF_FLAGS_DIRTY) == 0 && idewait(ide_chan) == 0) {
 		// The buffer is invalid. read from disk.
 		insl(PORT_DATA(ide_chan), buf->data, BLOCK_SIZE / 4);
@@ -134,15 +133,17 @@ static void ide_intr_handler(struct trap_frame *tf) {
 	// Make the buffer valid and remove the dirty flag.
 	buf->flags |= BUF_FLAGS_VALID;
 	buf->flags &= ~BUF_FLAGS_DIRTY;
-	
+
+	idequeue = buf->qnext;
+	buf->qnext = NULL;
+
 	// Wakeup the task wating for this buffer.
 	sem_signal(&ide_chan->sem);
-	
-	idequeue = buf->qnext;
+
 	if (idequeue != NULL) {
 		idestart(idequeue);
 	}
-	
+
 	spinlock_release(&idelock, &int_save);
 }
 
@@ -153,30 +154,27 @@ struct ide_channel *get_ide_channel(uint8_t nr) {
 
 void iderw(struct buf *buf) {
 	ASSERT(sem_holding(&buf->sem));
-	ASSERT((buf->flags & (BUF_FLAGS_VALID | BUF_FLAGS_DIRTY)) !=
-		   BUF_FLAGS_VALID);
+	ASSERT((buf->flags & (BUF_FLAGS_VALID | BUF_FLAGS_DIRTY)) != BUF_FLAGS_VALID);
 	bool int_save;
 	spinlock_acquire(&idelock, &int_save);
-	
-	struct buf **p;
-	for (p = &idequeue; *p != NULL; p = &(*p)->qnext)
-		/* Nothing to do */;
-	*p = buf;
-	if (idequeue == buf) {
-		// idequeue start from this buffer.
+
+	if (idequeue == NULL) {
+		idequeue = buf;
 		idestart(buf);
+	} else {
+		struct buf *prev;
+		for (prev = idequeue; prev->qnext != NULL; prev = prev->qnext)
+			;
+		prev->qnext = buf;
 	}
 
 	// Block self. Wait for request to finish.
-	while ((buf->flags & (BUF_FLAGS_VALID | BUF_FLAGS_DIRTY)) !=
-		   BUF_FLAGS_VALID) {
+	while ((buf->flags & (BUF_FLAGS_VALID | BUF_FLAGS_DIRTY)) != BUF_FLAGS_VALID) {
 		sem_wait(&buf->disk->ide_chan->sem);
 	}
-		
+
 	spinlock_release(&idelock, &int_save);
 }
-
-
 
 
 static void init_ide_chan(struct ide_channel *ide_chan) {
@@ -185,17 +183,17 @@ static void init_ide_chan(struct ide_channel *ide_chan) {
 
 	setup_irq_handler(ide_chan->irq_no, ide_intr_handler);
 	enable_irq(ide_chan->irq_no);
-	
+
 	for (int dev_no = 0; dev_no < 2; dev_no++) {
 		struct disk *hd = &ide_chan->devices[dev_no];
 		sprintf(hd->name, "hd%c", 'a' + dev_no);
 		hd->dev_no = dev_no;
-		hd->ide_chan = ide_chan;	
+		hd->ide_chan = ide_chan;
 
 		// Initialize sb and log in 'fs.c'(only initialize cur_disk).
 		hd->sb = NULL;
 		hd->log = NULL;
-		
+
 		// Don't use the main hard disk as a file system.
 		if (dev_no != 0 || ide_chan->ide_chan_id != 0) {
 			cur_disk = hd;
@@ -203,7 +201,7 @@ static void init_ide_chan(struct ide_channel *ide_chan) {
 	}
 }
 
-struct disk* get_current_disk() {
+struct disk *get_current_disk() {
 	return cur_disk;
 }
 
@@ -220,7 +218,7 @@ void ide_init() {
 	printk("ide_init start...\n");
 	spinlock_init(&idelock);
 	idequeue = NULL;
-	
+
 	uint8_t ide_chan_cnt = get_ide_channel_cnt();
 	for (uint8_t nr = 0; nr < ide_chan_cnt; nr++) {
 		init_ide_chan(&ide_chans[nr]);
@@ -231,7 +229,6 @@ void ide_init() {
 	}
 	printk("ide_init done...\n");
 }
-
 
 
 #ifdef __cplusplus
